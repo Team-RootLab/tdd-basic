5f12f0152f69330a95c26a83cb20580239d748b5
Junek3 <rlarudrms28@gmail.com>
2022-03-07 00:12:11 +0900

    first commit

---
+++ src/ch01/StringTest.java
@@ -0,0 +1,13 @@
+package ch01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringTest {
+	@Test
+	void substring() {
+		String str = "abcde";
+		assertEquals("cd", str.substring(2, 4));
+	}
+}
